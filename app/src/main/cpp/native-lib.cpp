24c969f73780ab659ebe4339cdd2e54628d62fdd
HemalAdani <hemaladani2711@gmail.com>
2019-07-21 14:26:13 -0400

    refactoring code

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,8 +7,7 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_sample_nativecodesample_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    int iSeed=10,iSecret;
-    iSecret=rand()%10;
+    int iSecret=rand()%10;
     std::string hello = "Hello from C++"+std::to_string(iSecret);
     return env->NewStringUTF(hello.c_str());
 }
